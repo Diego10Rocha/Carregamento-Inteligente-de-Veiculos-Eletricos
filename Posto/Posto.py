b9a0b515e9dd5b30eed83acda790a2536e4534d4
Carlos Valadão <61719667+arlosValadao@users.noreply.github.com>
2023-04-14 20:47:52 -0300

    Implementação Posto

---
+++ Posto/Posto.py
@@ -0,0 +1,69 @@
+from uuid import uuid4
+from random import randint
+import paho.mqtt.client as mqtt_client
+from consts import BROKER_ADDR, BROKER_PORT, GAS_STATION_TIME_TO_SEND, GAS_STATION_TIME_TO_REFRESH
+from time import sleep
+from threading import Thread
+
+
+# O posto vai criar um topico no broker
+# e enviar as informacoes da fila dele para
+# este topico
+
+# Cada posto tera um codigo (uuid4)
+# Um topico para cada posto
+# A mensagem enviada e um numero inteiro, que corresponde
+# a fila atual do posto
+# O posto envia a sua coordenada para os servidores da nevoa
+
+
+# O posto pertence a uma regiao pre-determinada
+# o programa cobrira no maximo 3 regioes
+
+
+# Apos iniciar, o posto so vai ficar enviando as informacoes da fila
+# dele para a nevoa para o topico dele. Dessa forma, a fila mudara a cada
+# X segundos
+
+def on_connect(rc: int) -> None:
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        raise Exception("Failure connect to Broker")
+
+
+class Posto:
+    def __init__(self) -> None:
+        self._id: str = str(uuid4())
+        self._region: int = randint(1, 3)
+        self._broker_addr: str = BROKER_ADDR
+        self._broker_port: int = BROKER_PORT
+        self._queue: int = randint(0, 100)
+        self._topic: str = 'gas_station' + '/' + self._id + '/' + 'region' + '/' + self._region.__str__()
+
+    def _connect_mqtt(self) -> mqtt_client:
+        client = mqtt_client.Client()
+        client.on_connect = on_connect
+        client.connect(self._broker_addr, self._broker_port)
+        return client
+
+    def _publish(self, client: mqtt_client) -> None:
+        while True:
+            sleep(GAS_STATION_TIME_TO_SEND)
+            topic = self._topic
+            msg = self._queue
+            result = client.publish(topic, msg)
+            status = result[0]
+            print('Failed to send message') if status else print('Successful send message')
+
+    def start(self) -> None:
+        self._connect_mqtt()
+        thread1 = Thread(target=self._publish)
+        thread2 = Thread(target=self._refresh_queue)
+        thread1.start()
+        thread2.start()
+
+    def _refresh_queue(self) -> None:
+        while True:
+            sleep(GAS_STATION_TIME_TO_REFRESH)
+            self._queue = randint(0, 100)
