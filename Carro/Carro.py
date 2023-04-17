2348228b7896248a4d416af0e0af554fcd733aa3
Diego10Rocha <diego93rocha@gmail.com>
2023-04-16 21:46:44 -0300

    resolve bugs

--- Carro/Carro.py
+++ Carro/Carro.py
@@ -3,11 +3,11 @@ import random
 import threading
 import time
 from uuid import uuid4
-from consts import BROKER_ADDR, BROKER_PORT, GAS_STATION_TIME_TO_SEND, GAS_STATION_TIME_TO_REFRESH
+from consts import BROKER_ADDR, BROKER_PORT
 from paho.mqtt import client as mqtt_client
 
 
-def on_connect(rc: int) -> None:
+def on_connect(client, userdata, flags, rc: int) -> None:
     if rc == 0:
         print("Connected to MQTT Broker!")
     else:
@@ -24,10 +24,10 @@ class Station:
 class Car:
     def __init__(self) -> None:
         self._id: str = str(uuid4())
-        self._region: int = random.randint(1, 3)
+        self._region: int = 1 #random.randint(1, 3)
         self._broker_addr: str = BROKER_ADDR
         self._broker_port: int = BROKER_PORT
-        self._battery_total_charge: int = 100
+        self._battery_total_charge: int = 10
         self._topic: str = 'gas_station' + '/+/' + 'region' + '/' + self._region.__str__()
         self._battery_level = self._battery_total_charge
         self._best_station: Station = None
@@ -46,9 +46,11 @@ class Car:
     def _subscribe(self, client: mqtt_client):
         def on_message(client, userdata, msg):
             print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-            station = json.loads(msg.payload.decode().replace("'", '"'))
-            if not self._best_station or self._best_station.queue > station.queue:
-                self._best_station = Station(station.id, station.region, station.queue)
+            station = json.loads(msg.payload.decode())
+            if not self._best_station or self._best_station.queue > station.get('queue', "Tamanho da fila não encontrado"):
+                self._best_station = Station(station.get('id', "Id não encontrado"),
+                                             station.get('region', "Região não encontrada"),
+                                             station.get('queue', "Tamanho da fila não encontrado"))
             # else:
             #     if self._best_station.queue > station.queue:
             #         self._best_station = Station(station.id, station.region, station.queue)
@@ -57,17 +59,22 @@ class Car:
         client.on_message = on_message
 
     def _battery_manager(self):
-        while self._batery_level != 0:
+        while self._battery_level != 0:
             time.sleep(1)
-            self._battery_level -= - 1
+            self._battery_level -= 1
 
-            if self._battery_level < self._battery_total_charge * 0.3:
+            if self._battery_level <= self._battery_total_charge * 0.3:
                 print("Bateria descarregada")
+                #implementar lógica de buscar um posto na nuvem
                 print("Se dirigindo ao posto com id:", self._best_station.id, "na região:", self._best_station.region)
+                time_to_recharge = self._best_station.queue * 1 + (self._battery_total_charge - self._battery_level)
+                time.sleep(time_to_recharge)
+                self._battery_level = self._battery_total_charge
+                print("O carro acabou de ser recarregado!")
         print("O carro morreu, impossível ir até um posto")
 
     def start(self):
-        thread1 = threading.Thread(target=self._mqtt_connect())
+        thread1 = threading.Thread(target=self._mqtt_connect)
 
         thread2 = threading.Thread(target=self._battery_manager)
 
@@ -76,3 +83,8 @@ class Car:
 
         # iniciar a thread de gerenciamento da bateria
         thread2.start()
+
+if __name__ == '__main__':
+    carro = Car()
+
+    carro.start()
---
+++ Carro/consts.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+BROKER_ADDR: Final = 'localhost'
+BROKER_PORT: Final = 1883
+GAS_STATION_TIME_TO_SEND: Final = 7
+GAS_STATION_TIME_TO_REFRESH: Final = 90
