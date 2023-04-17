bbbf4a4a659cf3d03918c7e19abb4957432320ef
Carlos Valadão <61719667+arlosValadao@users.noreply.github.com>
2023-04-16 21:57:12 -0300

    Define Constantes Importantes
    Constantes essenciais e necessárias para o bom funcionamento do programa

---
+++ consts/consts.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+BROKER_ADDR: Final = 'localhost'
+BROKER_PORT: Final = 1883
+GAS_STATION_TIME_TO_SEND: Final = 7
+GAS_STATION_TIME_TO_REFRESH: Final = 90
+EDGE_TIME_TO_SEND: Final = 10
