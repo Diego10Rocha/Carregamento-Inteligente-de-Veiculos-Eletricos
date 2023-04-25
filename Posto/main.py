f34872c117f571b46cc96743699ad1e9fc3b079c
arlosValadao <cvaladao@ecomp.uefs.br>
2023-04-25 14:45:46 -0300

    run posto

--- Posto/main.py
+++ Posto/main.py
@@ -10,7 +10,7 @@ def print_hi(name):
 
 def run_posto() -> None:
     my_posto = Posto()
-    Posto.start()
+    my_posto.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
