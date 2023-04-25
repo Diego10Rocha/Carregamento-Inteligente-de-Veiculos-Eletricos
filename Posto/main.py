bf0321e51c4cc60813afbf0765a67a13f05175e3
arlosValadao <cvaladao@ecomp.uefs.br>
2023-04-25 14:32:27 -0300

    run posto

--- Posto/main.py
+++ Posto/main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+from Posto import Posto
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -8,9 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def run_posto() -> None:
+    my_posto = Posto()
+    Posto.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run_posto()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
