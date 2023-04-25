d6efd21edf301633a701c120404fb8b4cc688ca5
Diego10Rocha <diego93rocha@gmail.com>
2023-04-24 21:46:58 -0300

    logica do carro completa

--- Carro/consts.py
+++ Carro/consts.py
@@ -1,6 +1,20 @@
 from typing import Final
 
-BROKER_ADDR: Final = 'localhost'
-BROKER_PORT: Final = 1883
+REGION_1: Final = 1
+REGION_2: Final = 2
+REGION_3: Final = 3
+
+DEFAULT_RECV_TCP_BYTES: Final = 1024
+
+BROKER_REGION_1_ADDR: Final = "localhost"
+BROKER_REGION_1_PORT: Final = 1883
+
+BROKER_REGION_2_ADDR: Final = "localhost"
+BROKER_REGION_2_PORT: Final = 1883
+
+BROKER_REGION_3_ADDR: Final = "localhost"
+BROKER_REGION_3_PORT: Final = 1883
+
 GAS_STATION_TIME_TO_SEND: Final = 7
 GAS_STATION_TIME_TO_REFRESH: Final = 90
+EDGE_TIME_TO_SEND: Final = 10
